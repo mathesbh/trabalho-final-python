e88acaf18e8cf6096696c6c88da92070c8ed77e9
Matheus Salomão <mathesbh@gmail.com>
2021-06-21 16:48:06 -0300

    Inclui rota contas a receber

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('category/create', views.create_category, name='create_category'),
     path('pay_bills/create', views.create_pay_bills, name='create_pay_bills'),
+    path('receive_bills/create', views.create_receive_bills, name='create_receive_bills'),
 ]
