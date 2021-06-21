3acbe4202efff55d05f11a094fdc65cfbfe42ba4
Matheus Salomão <mathesbh@gmail.com>
2021-06-21 16:08:52 -0300

    Inclui rota criar contas a pagar

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,9 @@
+from my_app.views.pay_bills import create_pay_bills
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('category/create', views.create_category, name='create_category')
+    path('category/create', views.create_category, name='create_category'),
+    path('pay_bills/create', views.create_pay_bills, name='create_pay_bills'),
 ]
