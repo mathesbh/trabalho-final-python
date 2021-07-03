e2ba95384c91786a5b7ee1e97521bae4c77405dc
Matheus Salomão <mathesbh@gmail.com>
2021-07-03 13:57:40 -0300

    Adiciona metodos para manipular manager dos modelos contas a pagar e receber

--- my_app/models/pay_bills.py
+++ my_app/models/pay_bills.py
@@ -1,8 +1,31 @@
+from my_app.models import category
 from django.db import models
+from django.db.models.aggregates import Sum
 from . import Category
 
 STATUS_CHOICE = (('N', 'a Pagar'), ('S','Pago'))
 PAYMENT_CHOICE = (('D','Dinheiro'), ('CD','Débito'), ('CC','Crédito'), ('B','Boleto'))
+
+class PayBillsManager(models.Manager):
+  def get_sum_pay_month(self, month, year):
+    sum_month = float(PayBills.objects.filter(due_date__month=month, due_date__year=year).aggregate(Sum('value'))['value__sum'] or 0)
+    return sum_month
+
+  def get_sum_pays_category(self, month, year):
+    categories = Category.objects.all().count()
+    pays = []
+
+    for i in range(1, categories):
+      sum_pays = float(PayBills.objects.filter(due_date__month=month, due_date__year=year, category=i).aggregate(Sum('value'))['value__sum'] or 0)
+      pay = {
+        'category': Category.objects.get(id=i),
+        'sum_pays': sum_pays
+      }
+      if sum_pays > 0:
+        pays.append(pay)
+      
+    return pays
+       
 
 class PayBills(models.Model):
   value = models.FloatField()
@@ -12,3 +35,4 @@ class PayBills(models.Model):
   due_date = models.DateField(null=True, blank=True)
   pay_date = models.DateField(null=True, blank=True)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  objects = PayBillsManager()
--- my_app/models/receive_bills.py
+++ my_app/models/receive_bills.py
@@ -1,10 +1,33 @@
 from django.db import models
+from django.db.models.aggregates import Sum
 from . import Category
 
 STATUS_CHOICE = (('N','a Receber'), ('S','Recebido'))
+
+class ReceiveBillsManager(models.Manager):
+  def get_sum_receive_month(self, month, year):
+    sum_month = float(ReceiveBills.objects.filter(receive_date__month=month, receive_date__year=year).aggregate(Sum('value'))['value__sum'] or 0)
+    return sum_month
+  
+  def get_sum_receives_category(self, month, year):
+    categories = Category.objects.all().count()
+    receives = []
+
+    for i in range(1, categories):
+      sum_receives = float(ReceiveBills.objects.filter(receive_date__month=month, receive_date__year=year, category=i).aggregate(Sum('value'))['value__sum'] or 0)
+      pay = {
+        'category': Category.objects.get(id=i),
+        'sum_receives': sum_receives
+      }
+      if sum_receives > 0:
+        receives.append(pay)
+      
+    return receives
+
 class ReceiveBills(models.Model):
   value = models.FloatField()
   description = models.CharField(max_length=200, null=True, blank=True)
   status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='N')
   receive_date = models.DateField()
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  objects = ReceiveBillsManager()
