ba12b549a3baef648fea992a006283f761181369
Matheus Salomão <mathesbh@gmail.com>
2021-07-03 13:58:40 -0300

    - Modifica view e template fluxo de caixa para listar - Soma por mês e categorias - Mostrar saldo

--- my_app/views/cash_flow.py
+++ my_app/views/cash_flow.py
@@ -12,34 +12,32 @@ def cash_flow(request: HttpRequest):
   else:
     data = request.POST
     initial_balance = data['initial_balance']
-    start_date = data['start_date']
-    end_date = data['end_date']
-    pay_bills_due_date = PayBills.objects.filter(due_date__range=(start_date, end_date))
-    receive_bills_date = ReceiveBills.objects.filter(receive_date__range=(start_date, end_date))
-
-    total_pay_bills = 0
-    total_receive_bills = 0
-
-    total = 0
-
-    for pay_bills in pay_bills_due_date:
-      total_pay_bills += pay_bills.value
-
-    for receive_bills in receive_bills_date:
-      total_receive_bills += receive_bills.value
-
-    total = (total_receive_bills - total_pay_bills) + float(initial_balance)
-   
+    start_date = data['start_date'].split('-')
+    end_date = data['end_date'].split('-')
+    start_month = int(start_date[1])
+    end_month = int(end_date[1])
+    year = int(start_date[0])
+    result = []
+    
+    while start_month <= end_month:
+      month = {
+        'month': start_month,
+        'pay_bills':{
+          'sum_month': PayBills.objects.get_sum_pay_month(month=start_month, year=year),
+          'sum_categories': PayBills.objects.get_sum_pays_category(month=start_month, year=year),
+        },
+        'receive_bills':{
+          'sum_month': ReceiveBills.objects.get_sum_receive_month(month=start_month, year=year),
+          'sum_categories': ReceiveBills.objects.get_sum_receives_category(month=start_month, year=year),
+        },
+        'saldo': float(initial_balance) - PayBills.objects.get_sum_pay_month(month=start_month, year=year) + ReceiveBills.objects.get_sum_receive_month(month=start_month, year=year)
+      }
+      start_month += 1
+      result.append(month)   
 
     return render(request, 'finances/cash_flow/report.html', {
       'initial_balance': initial_balance,
-      'start_date': start_date,
-      'end_date': end_date,
-      'pay_bills': pay_bills_due_date,
-      'receive_bills': receive_bills_date,
-      'total_pay_bills': total_pay_bills,
-      'total_receive_bills': total_receive_bills,
-      'total': total,      
+      'result': result,    
     })
 
     
